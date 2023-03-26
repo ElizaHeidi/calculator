ae6d1b2cdb8b5902c0351c3d6d8482bf958f0c59
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2023-03-26 12:23:41 -0500

    Refactor code to include currentValue, previousValue, and operator

--- main.js
+++ main.js
@@ -4,87 +4,78 @@ const p = document.querySelector('p');
 const clear = document.querySelector('[data-clear]');
 const addButton = document.querySelector('.add')
 const equals = document.querySelector('[data-equals');
-const operators = document.querySelectorAll('[data-operation]');
+let operators = document.querySelectorAll('[data-operation]');
 const dot = document.querySelector('dot');
-let operand = '';
-let arg1 = '';
-let arg2 = '';
+let previousOutput = document.querySelector('.previousNumber');
+let currentOutput = document.querySelector('.currentNumber');
+let operator = '';
+let currentValue = '';
+let previousValue = '';
 
 // Math operations
 
 function add(...nums) {
-let resultAdd = nums.reduce((num1, num2) => num1 + num2)
-// First, clear the output, then append result to output
+    let resultAdd = nums.reduce((num1, num2) => num1 + num2)
+    // First, clear the output, then append result to output
+        output.innerText = '';
+        output.innerText += Number(resultAdd);
+    }
+    
+    // console.log(add(4, 4));
+    
+    function subtract(...nums) {
+    let resultSubtract = nums.reduce((num1, num2) => num1 - num2)
     output.innerText = '';
-    output.innerText += Number(resultAdd);
-}
+    output.innerText += Number(resultSubtract);
+    }
+    
+    // console.log(subtract(4, 4));
+    
+    function multiply(...nums) {
+    let resultMultiply = nums.reduce((num1, num2) => num1 * num2)
+    output.innerText = '';
+    output.innerText += Number(resultMultiply);
+    }
+    
+    // console.log(multiply(4, 4));
+    
+    function divide(...nums) {
+    let resultDivide = nums.reduce((num1, num2) => num1 / num2)
+    output.innerText = '';
+    output.innerText += Number(resultDivide);
+    }
 
-// console.log(add(4, 4));
-
-function subtract(...nums) {
-let resultSubtract = nums.reduce((num1, num2) => num1 - num2)
-output.innerText = '';
-output.innerText += Number(resultSubtract);
-}
-
-// console.log(subtract(4, 4));
-
-function multiply(...nums) {
-let resultMultiply = nums.reduce((num1, num2) => num1 * num2)
-output.innerText = '';
-output.innerText += Number(resultMultiply);
-}
-
-// console.log(multiply(4, 4));
-
-function divide(...nums) {
-let resultDivide = nums.reduce((num1, num2) => num1 / num2)
-output.innerText = '';
-output.innerText += Number(resultDivide);
-}
-
-// console.log(divide(4, 4));
-
-
-// append numbers to input on click
+/* For each num button, output the button's innertext when clicked, 
+and add the textContent of button to currentValue
+*/  
 
 buttonNums.forEach((buttonNum) => {
     buttonNum.addEventListener('click', (e) => {
-        output.innerText += e.target.innerText;
+        currentOutput.innerText += e.target.innerText;
+        getNumber(e.target.textContent)
         });
     });
 
+function getNumber(num) {
+    currentValue += num;
+}
+
+   /* For each operator button, input its value to the operator variable, 
+set the currentValue to the previousValue (so make it smaller at the top of the screen
+plus a ' ' plus the operator (currentValue to `${prevValue}` + ' ' + `${operator}`)  
+and then set the current output to the new currentValue     
+*/
+
 operators.forEach((operator) => {
     operator.addEventListener('click', (e) => {
-        output.innerText += e.target.innerText;
-        const value = e.target.innerText;
+        previousOutput.innerText = previousValue + ' ' + operator;
+        currentOutput.innerText = currentValue;
 
-    // if e.target.innerText = '=', then 
-    // split innerText into an array: ['number', 'number +']
-    // then call operate(), and set the operand to 
-    // operate(+, number, number +)     
-
-        if (value === '=') {
-          if (output.innerText.includes('+')) {
-            const splitInput = output.innerText.split('+');
-            console.log(output.innerText.split('+'));
-            console.log(splitInput[1]);
-            operate('+', splitInput[0], splitInput[1]);
-          }  
-          if (output.innerText.includes('-')) {
-            const splitInput = output.innerText.split('-')
-            operate('-', splitInput[0], splitInput[1]);
-          }  
-          if (output.innerText.includes('*')) {
-            const splitInput = output.innerText.split('*')
-            operate('*', splitInput[0], splitInput[1]);
-          }  
-          if (output.innerText.includes('/')) {
-            const splitInput = output.innerText.split('/')
-            operate('/', splitInput[0], splitInput[1]);
-          }  
-          
-        }
+        // if (currentOutput.innerText === '=') {
+        //  operate();
+        //  previousOutput.innerText = '';
+        //  currentOutput.innerText = previousValue;  
+        // }
         });
     });
 
@@ -93,20 +84,21 @@ operators.forEach((operator) => {
 
     function operate(operand, arg1, arg2) {
         // console.log(arg1, operand, arg2);
-    const value1 = parseFloat(arg1);
-    const value2 = parseFloat(arg2);
+        previousValue = Number(previousValue);
+        currentValue = Number(currentValue)
+
         if (operand === '+') {
-          add(value1, value2);
+          add(previousValue, currentValue);
                    
         }
         if (operand === '-') {
-            subtract(value1, value2);
+            subtract(previousValue, currentValue);
         }
         if (operand === '*') {
-            multiply(value1, value2);
+            multiply(previousValue, currentValue);
         }
         if (operand === '/') {
-            divide(value1, value2);
+            divide(previousValue, currentValue);
         }
        
     }
@@ -114,14 +106,14 @@ operators.forEach((operator) => {
 // clear all output 
 
 clear.addEventListener('click', () => {
-    output.innerText = '';
+    previousValue = '';
+    currentValue = '';
+    operator = '';    
+    previousOutput.innerText = currentValue;
+    currentOutput.innerText = currentValue;
     })
 
-function decimal() {
-    if (output.textContent.includes('.')) {
-        output.textContent += '.'
-    }
-}    
+
 
 
      
